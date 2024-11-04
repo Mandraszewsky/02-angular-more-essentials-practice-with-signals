0f73b1629155d74c954f3ac5109f2b6071e19f68
Mandraszewsky <mandrah@o2.pl>
2024-11-04 16:23:07 +0100

    migrate standalone components to angular modules

--- src/app/investment-results/investment-results.component.ts
+++ src/app/investment-results/investment-results.component.ts
@@ -1,12 +1,10 @@
-import { CurrencyPipe } from '@angular/common';
 import { Component, inject, computed } from '@angular/core';
 
 import { InvestmentService } from '../investment.service';
 
 @Component({
   selector: 'app-investment-results',
-  standalone: true,
-  imports: [CurrencyPipe],
+  standalone: false,
   templateUrl: './investment-results.component.html',
   styleUrl: './investment-results.component.css'
 })
